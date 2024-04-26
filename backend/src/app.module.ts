9a522bd38256ee21c0b9a7b73258bc0aebe6c3d1
yoounyoungheon <iddyoon@gmail.com>
2024-04-26 17:37:02 +0900

    [api] feat: auth module 생성

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -13,7 +14,8 @@ import { TypeOrmConfigService } from './typeorm.config';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useClass: TypeOrmConfigService
-    })
+    }),
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
