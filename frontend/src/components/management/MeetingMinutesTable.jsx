e7b7abedf85f8e345ee2c8c9c97cd6e42ba10b45
swimkim <81215099+swim-kim@users.noreply.github.com>
2024-05-06 22:11:21 +0900

    feat:meeting minutes레이아웃

--- frontend/src/components/management/MeetingMinutesTable.jsx
+++ frontend/src/components/management/MeetingMinutesTable.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import Product from './Product';
+import Minute from './Minute';
 
 const TableContainer = styled.div`
-  width: 100%;
-  margin: auto;
-  margin-top: 20px;
+    width: 100%;
+    margin: auto;
+    margin-top: 20px;
 `;
 
 const Table = styled.table`
-  width: 96%;
-  border-collapse: collapse;
-  margin: auto;
+    width: 96%;
+    border-collapse: collapse;
+    margin: auto;
 `;
 
 const TableRow = styled.tr`
-  border-bottom: 1px solid #ccc;
-  height: 30px;
+    border-bottom: 1px solid #ccc;
+    height: 30px;
 `;
 
 const TableCell = styled.td`
@@ -29,24 +29,74 @@ const TableCell = styled.td`
     width: ${props => props.width || 'auto'}; 
 `;
 
+const PaginationContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+  position: fixed;
+  bottom: 20px; 
+  width: 100%;
+  z-index: 1000; 
+`;
 
-function ProductTable() {
-  return (
-    <TableContainer>
-      <Table>
-        <tbody>
-          <TableRow>
-            <TableCell width="5%">ID</TableCell>
-            <TableCell width="13%">Image</TableCell>
-            <TableCell width="50%">Product</TableCell>
-            
-          </TableRow>
-        </tbody>
-        <Product />
-        <Product />
-      </Table>
-    </TableContainer>
-  );
+const PaginationButton = styled.button`
+  margin: 0 5px;
+  padding: 5px 10px;
+  font-size: 14px;
+  font-family: "Wanted Sans";
+  cursor: pointer;
+  border: none;
+  background-color:${({ active }) => active ? 'transparent' : 'transparent'};
+  color: ${({ active }) => active ? '#1C3159' : ' #D9D9D9'};
+  border-radius: 5px;
+`;
+
+function MeetingMinutesTable() {
+    const [currentPage, setCurrentPage] = useState(1);
+    const minutesPerPage = 13;
+
+  
+    const minutes = [Minute,Minute];
+
+    const totalPages = Math.ceil(minutes.length / minutesPerPage);
+    const indexOfLastMinute = currentPage * minutesPerPage;
+    const indexOfFirstMinute = indexOfLastMinute - minutesPerPage;
+    const currentMinutes = minutes.slice(indexOfFirstMinute, indexOfLastMinute);
+
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    return (
+        <>
+            <TableContainer>
+                <Table>
+                <tbody>
+                    <TableRow>
+                        <TableCell width="10%">ID</TableCell>
+                        <TableCell width="60%">Title</TableCell>
+                        <TableCell width="30%">File</TableCell>     
+                    </TableRow>
+                    {currentMinutes.map((mintue, index) => (
+                    <Minute key={index} {...minutes} />
+                    ))}
+                </tbody>
+                <Minute />
+            </Table>
+            </TableContainer>
+            <PaginationContainer>
+                {Array.from({ length: totalPages }, (_, index) => (
+                    <PaginationButton
+                        key={index}
+                        active={currentPage === index + 1}
+                        onClick={() => handlePageChange(index + 1)}
+                    >
+                        {index + 1}
+                    </PaginationButton>
+                ))}
+        </PaginationContainer>
+      </>
+    );
 }
 
-export default ProductTable;
+export default MeetingMinutesTable;
