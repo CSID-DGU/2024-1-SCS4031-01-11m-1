d0f2457b3b39de2369d1997e69019b8769d6028a
Seulgigi <2021112414@dgu.ac.kr>
2024-05-17 04:25:19 +0900

    [LLM] feat: sentiment analysis for each category

--- llm/main.js
+++ llm/main.js
@@ -1,19 +1,26 @@
 import fs from "fs";
 import { category_classifier } from "./category_classifier.js";
+import { sentiment_analysis } from "./sentiment_analysis.js";
 
 // 제품 하나에 대한 전체 리뷰
 const reviews = JSON.parse(
   fs.readFileSync("llm/test_data/test_data.json", "utf8")
 );
-reviews["카테고리"] = null;
-reviews["감성"] = null;
+reviews["카테고리"] = {};
 
 // 사용자가 입력한 카테고리
 const categories = ["디자인", "향", "편의성", "발림성", "제형", "가격"];
 
 (async () => {
   for (const review of reviews) {
-    // 카테고리가 리스트로 반환
-    review["카테고리"] = await category_classifier(review["리뷰"], categories);
+    const review_category = await category_classifier(
+      review["리뷰"],
+      categories
+    );
+    review["카테고리"] = await sentiment_analysis(
+      review["리뷰"],
+      review_category
+    );
+    // { "카테고리1": "긍정/부정", ... }
   }
 })();
