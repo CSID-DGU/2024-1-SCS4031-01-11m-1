d1d79f1ead4809bab3fb5b6577608ae3dc7635b2
Seulgigi <2021112414@dgu.ac.kr>
2024-05-17 16:27:00 +0900

    [LLM] feat: set keyword extraction and RAG

--- llm/README.md
+++ llm/README.md
@@ -26,3 +26,10 @@ PDFLoader
 ```
 npm install pdf-parse
 ```
+
+### 수정 필요한 부분
+
+1. sentiment prompt - 현재는 예외 처리 ..
+
+- 입력한 카테고리 외 카테고리가 함께 반환됨
+- '중립'이 함께 답변 -> '긍정', '부정'으로만 답변
--- llm/openai/main.js
+++ llm/openai/main.js
@@ -11,6 +11,12 @@ reviews["카테고리"] = {};
 // 사용자가 입력한 카테고리
 const categories = ["디자인", "향", "편의성", "발림성", "제형", "가격"];
 
+// review를 카테고리/긍부정으로 나눠 저장할 배열 초기화
+const categoryReview = {};
+categories.forEach((category) => {
+  categoryReview[category] = { 긍정: [], 부정: [] };
+});
+
 (async () => {
   for (const review of reviews) {
     const review_category = await category_classifier(
@@ -22,6 +28,15 @@ const categories = ["디자인", "향", "편의성", "발림성", "제형", "가
       review_category
     );
     // { "카테고리1": "긍정/부정", ... }
-    console.log(review["카테고리"]);
+    // console.log(review["카테고리"]);
+
+    // 키워드 도출을 위해서 카테고리별 긍정/부정 리뷰 데이터들을 categoryReview에 저장
+    for (const [category, sentiment] of Object.entries(review["카테고리"])) {
+      if (categoryReview[category] && categoryReview[category][sentiment]) {
+        categoryReview[category][sentiment].push(review["리뷰"]);
+      }
+    }
   }
+  // { "카테고리1": { "긍정": ["리뷰", "리뷰", ...], "부정": ["리뷰", "리뷰", ...] }, "카테고리2": { ... }, ... }
+  // console.log(categoryReview);
 })();
