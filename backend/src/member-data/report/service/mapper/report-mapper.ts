67b07c1b7d8e4b543555453b7db3df189bbdd66a
yoounyoungheon <107925716+yoounyoungheon@users.noreply.github.com>
2024-06-05 15:45:23 +0900

    [BE] report List

---
+++ backend/src/member-data/report/service/mapper/report-mapper.ts
@@ -0,0 +1,9 @@
+import { ReportEntiy } from "../../entity/report.entity";
+import { ReportListDto } from "../dto/report-list.dto";
+
+export class ReportMapper{
+  static reportEntityToReportListDto(reportEntiy: ReportEntiy): ReportListDto{
+    const reportListDto = ReportListDto.createNew(reportEntiy.id, reportEntiy.productName, reportEntiy.createdAt, reportEntiy.updatedAt)
+    return reportListDto;
+  }
+}
