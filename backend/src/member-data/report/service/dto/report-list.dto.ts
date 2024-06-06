f3f30745c4a75624049c6d5dac40ffcf1f0b5c7a
Skyline5555 <john3176@dongguk.edu>
2024-06-06 20:24:28 +0900

    레포트 리스트 로컬 시간으로 변경

--- backend/src/member-data/report/service/dto/report-list.dto.ts
+++ backend/src/member-data/report/service/dto/report-list.dto.ts
@@ -2,14 +2,14 @@ export class ReportListDto{
   // 레포트id, 날짜, 상품명, 중요도
   id: string;
   productName: string;
-  createdAt: Date;
-  updatedAt: Date;
+  createdAt: string;
+  updatedAt: string;
 
   constructor(id: string, productNama: string, createdAt: Date, updatedAt: Date){
     this.id = id;
     this.productName = productNama;
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
+    this.createdAt = createdAt.toLocaleString();
+    this.updatedAt = updatedAt.toLocaleString();
   }
 
   static createNew(id: string, productNama: string, createdAt: Date, updatedAt: Date){
